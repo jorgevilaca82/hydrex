b0841658a7c58fb902117704dbf2f32c247c5d33
Jorge Vilaça <jorge.vilaca@gmail.com>
2025-07-04 15:02:26 -0300

    prepare for publishing

--- mix.exs
+++ mix.exs
@@ -6,11 +6,7 @@ defmodule Hydrex.MixProject do
       app: :hydrex,
       version: "0.1.0",
       elixir: "~> 1.18",
-      description: "Hydrex is a library for hydrating Ecto structs with virtual fields.",
-      licenses: ["MIT"],
-      links: %{
-        "GitHub" => "https://github.com/jorgevilaca82/hydrex"
-      },
+      package: package(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -23,11 +19,24 @@ defmodule Hydrex.MixProject do
     ]
   end
 
+  defp package do
+    [
+      name: "hydrex",
+      description: "Hydrex is a library for hydrating Ecto structs with virtual fields.",
+      maintainers: ["Jorge Vilaca"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/jorgevilaca82/hydrex"
+      }
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
