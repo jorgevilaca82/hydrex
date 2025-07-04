0cd1896a17b7b6f67758f34ffa8a68261d6cf4c9
Jorge Vilaça <jorge.vilaca@gmail.com>
2025-07-04 15:26:40 -0300

    maybe_hydrate boolean version

--- lib/hydrex/utils.ex
+++ lib/hydrex/utils.ex
@@ -1,7 +1,17 @@
 defmodule Hydrex.Hydratable.Utils do
   alias Hydrex.Hydratable
 
-  def maybe_hydrate(data, opts) do
+  @doc """
+  Conditionally hydrates the given data.
+
+  If the second argument is a boolean, hydration is performed if it's `true`.
+  If the second argument is a keyword list, hydration is performed if `:hydrate` is `true` in the options.
+  """
+  @spec maybe_hydrate(any(), boolean() | keyword()) :: any()
+  def maybe_hydrate(data, true), do: Hydratable.hydrate(data)
+  def maybe_hydrate(data, false), do: data
+
+  def maybe_hydrate(data, opts) when is_list(opts) do
     hydrate = Keyword.get(opts, :hydrate, false)
 
     case hydrate do
